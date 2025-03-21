499f5b9d07dd722d84dba0478c6893fcba2ad256
DavidHashBrownQiu <longhutch@gmail.com>
2025-03-21 10:16:43 -0400

    ytrrtytrhtryrtytrytrytrhtrytyrtrytryrtytyrtrytyrtrytyrtyrtrytryrtyytrtyrtrytyrtryrtytryytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytyytytytytytytytytytytytytytytytytytytytytytytytytytytyytytyththththt

---
+++ flashcards.py
@@ -0,0 +1,16 @@
+import json
+
+class Car:
+    def __init__(self, make, model, year, image=None):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.image = image
+    
+    def display_info(self):
+        print(f"{self.year} {self.make} {self.model}")
+    
+    def to_dict(self):
+        return {"make": self.make, "model": self.model, "year": self.year, "image": self.image}
+    
+Car.display_info("1989")
