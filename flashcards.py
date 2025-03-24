53421f997040f07003fc863b721e19d8c72d4391
DavidHashBrownQiu <longhutch@gmail.com>
2025-03-24 10:17:13 -0400

    trytsjrdtrjtrjjfhghfgfhghfghfhhfhghghhhgf

--- flashcards.py
+++ flashcards.py
@@ -12,5 +12,32 @@ class Car:
     
     def to_dict(self):
         return {"make": self.make, "model": self.model, "year": self.year, "image": self.image}
+
+"""my_car = Car("Toyota", "Camry", 2023, "car1.jpg")
+print(my_car.display_info())  # Output: 2023 Toyota Camry"""
+
+cars = [
+    Car("Toyota", "Camry", 2023, "camry_image.jpg"), #Examples of instances in classes
+    Car("Honda", "Civic", 2022, "civic_image.jpg"),
+    Car("Ford", "Mustang", 2021, "mustang_image.jpg")
+]
+
+# Convert objects to dictionaries
+cars_data = [car.to_dict() for car in cars]
+print(cars_data)
+
+# Save to a JSON file
+with open("cars.json", "w") as file:
+    json.dump(cars_data, file, indent=4)
+
+
+
+"""class Mynumber:
+    def __init__(self, value):
+        self.value = value
     
-Car.display_info("1989")
+    def print_value(self):
+        print(self.value)
+
+obj1 = Mynumber(170)
+obj1.print_value()"""
